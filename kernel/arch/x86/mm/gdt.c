fbea3b502cf309180001e7f441c4738a6c60cffc
Isabell Cowan <isabellcowan@gmail.com>
2017-05-06 17:29:37 -0500

    GDT now reloactes itself on register using malloc.

--- kernel/arch/x86/mm/gdt.c
+++ kernel/arch/x86/mm/gdt.c
@@ -3,7 +3,10 @@
 #include <izixboot/gdt.h>
 #include <izixboot/gdt32.h>
 
+#include <string.h>
+
 #include <kprint/kprint.h>
+#include <mm/malloc.h>
 
 static gdt_register_t *gdtr;
 
@@ -45,15 +48,38 @@ static inline void print_logical_entry (gdt32_logical_entry_t logical_entry) {
 		logical_entry.flags.granularity ? "GR" : "--");
 }
 
+static void __attribute__((noinline)) gdt_reload () {
+	asm(
+		"	lgdt	(%0);\n"
+		"	ljmp	$0x08,		$gdt_reload_target;\n"
+		"gdt_reload_target:\n"
+		"	nop;\n"
+		:
+		:"r"(gdtr));
+}
+
 void gdt_register (gdt_register_t *registry) {
-	gdtr = registry;
+	gdt32_logical_register_t logical_registry = get_logical_gdtr (*registry);
+
+	const size_t gdt_entries_length = logical_registry.size * sizeof(gdt32_entry_t);
+
+	gdt32_entry_t *new_entries = malloc (gdt_entries_length);
+	gdt_register_t *new_registry = malloc (sizeof(gdt_register_t));
+
+	memcpy (new_entries, logical_registry.offset, gdt_entries_length);
+
+	logical_registry.offset = new_entries;
+	*new_registry = gdt32_register_encode (logical_registry);
+
+	gdtr = new_registry;
+
+	gdt_reload ();
 }
 
 void gdt_dump_entries () {
 	size_t i;
 
 	gdt32_logical_register_t logical_registry = get_logical_gdtr (*gdtr);
-
 
 	kputs ("mm/gdt: GDT registry:\n");
 	print_logical_gdtr (logical_registry);
@@ -76,6 +102,5 @@ void gdt_dump_entries () {
 		}
 	}
 }
-
 
 // vim: set ts=4 sw=4 noet syn=c:
